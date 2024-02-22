ce748965bae366601562c5e8986378e11b374299
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2024-02-22 12:59:20 -0300

    Update app.js

--- HTML AULA 2024/app.js
+++ HTML AULA 2024/app.js
@@ -1,9 +1,24 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número de 1 a 10'
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número de 1 a 10'
 
-function verificarChute(){
+function verificarChute() {
     alert('Botão Clicado')
 }
+
+function exibirTextoNaTela(tag, texto) {
+    let titulo = document.querySelector(tag);
+    titulo.innerHTML = texto;
+}
+
+exibirTextoNaTela("h1", 'Jogo do amigo secreto')
+exibirTextoNaTela("p", 'Escolha um número de 1 a 10')
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1)
+}
+
+numero=gerarNumeroAleatorio()
+exibirTextoNaTela("p", numero)
