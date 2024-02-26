9a69fb14e9697ffc36b05184e7e8564ab58457a6
Guerreiro015 <antoniolucasluana@gmail.com>
2024-02-25 22:08:46 -0300

    Update inclusao.js

--- CRUD HTML/inclusao.js
+++ CRUD HTML/inclusao.js
@@ -1,7 +1,33 @@
-let nomes = document.getElementById('nome').value
-let senhas = document.getElementById('senha').value
+const request = window.indexedDB.open("MeuBancoDeTeste", 3);
 
-var db = openDatabase('meu_banco')
-db = transaction(function(incluir){
-    incluir.executeSql("INSERT INTO usuarios(nome,senha) VALUES(?,?)",[nomes,senhas])
-})
+   request.onsuccess = function () {
+     console.log("Banco de dados aberto com sucesso");
+     const db = request.result;
+     const transaction = db.transaction("carros", "readwrite");
+     const store = transaction.objectStore("carros");
+
+     // Exemplo: Adicionando dados à loja de objetos
+     store.put({ id: 1, cor: "Vermelho", marca: "Toyota" });
+     store.put({ id: 2, cor: "Vermelho", marca: "Kia" });
+     store.put({ id: 3, cor: "Azul", marca: "Honda" });
+     store.put({ id: 4, cor: "Prata", marca: "Subaru" });
+
+     // Exemplo: Recuperando dados da loja de objetos
+     const idQuery = store.get(4);
+     const corQuery = store.index("carros_cor").getAll(["Vermelho"]);
+     const corEMarcaQuery = store.index("cor_e_marca").get(["Azul", "Honda"]);
+
+     idQuery.onsuccess = function () {
+       console.log("Resultado da consulta por ID:", idQuery.result);
+     };
+     corQuery.onsuccess = function () {
+       console.log("Resultado da consulta por cor:", corQuery.result);
+     };
+     corEMarcaQuery.onsuccess = function () {
+       console.log("Resultado da consulta por cor e marca:", corEMarcaQuery.result);
+     };
+
+     transaction.oncomplete = function () {
+       db.close();
+     };
+   };
