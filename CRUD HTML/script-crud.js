ed99bfd44279e523ca6c1224ac466fe0e20fceb5
Guerreiro015 <antoniolucasluana@gmail.com>
2024-02-26 21:19:05 -0300

    ATULZAR

---
+++ CRUD HTML/script-crud.js
@@ -0,0 +1,19 @@
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
+const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+const textarea = document.querySelector('.app__form-textarea')
+let tarefas = []
+
+btnAdicionarTarefa.addEventListener('click', () => {
+    formAdicionarTarefa.classList.toggle('hidden')
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const descricaoTarefa = textarea.value
+    const tarefa = {
+        descricao: textarea.value
+    }
+    tarefas = push(tarefa)
+
+    localStorage.setItem('tarefas', tarefas)
+})
