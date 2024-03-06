7e9b5cd24780fe177ae564e6097761a211d074b3
Guerreiro015 <antoniolucasluana@gmail.com>
2024-03-06 20:15:46 -0300

    atualizar

--- DANKI-CODE/mongo/calculoGeral.js
+++
@@ -1,8 +0,0 @@
-
-
-data = document.getElementsByTagName('th')
-data[2].innerHTML = 'OLA MUNDO'
-data[0].innerHTML = "Conteúdo Manipulado"
-
-//------------------------------------------------------------------------
-
