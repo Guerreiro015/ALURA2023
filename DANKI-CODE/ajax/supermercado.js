6dddd05c63a7764632c351e9e16cf2a060d6f315
Guerreiro015 <antoniolucasluana@gmail.com>
2024-03-15 21:59:55 -0300

    atualizar

---
+++ DANKI-CODE/ajax/supermercado.js
@@ -0,0 +1,10 @@
+var item = [];
+
+document.querySelector('input[type=submit]').addEventListener(function(){
+    var nomeProduto=document.querySelector(input[id=nome-produto]).value
+    var valorProduto=document.querySelector(input[id=valor-produto]).value
+
+    alert(nomeProduto)
+    alert(valorProduto)
+})
+
