c08c6aa8ccccaff5acc49c4a2bb4850866e67c0c
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2024-04-09 16:01:23 -0300

    atualizar

--- EXCEL-JAVASCRIPT/JAVA_EXCEL/plani.js
+++ EXCEL-JAVASCRIPT/JAVA_EXCEL/plani.js
@@ -1,15 +1,16 @@
 
 const XLSX = require("xlsx");       
 
-const file = 'sindicato.xlsx';
+const file = 'relatorio.xlsx';
 const workbook = XLSX.readFile(file);
 const sheetName = workbook.SheetNames[0];
 const worksheet = workbook.Sheets[sheetName];
 const data = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
 
-// console.log(data);
-// console.log(data[5]);
+console.log(data);
+
 
 for(i=0;i<data.length;i++){
-    console.log('ID: ',data[i][0],'Nome:',data[i][1]);
+    if(data[i][4]=='OPERAÇÃO')
+    console.log(data[i][4],'--',data[i][9],'--',data[i][10]);
 };
