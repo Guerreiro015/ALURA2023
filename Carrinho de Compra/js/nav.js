6d2f7c6d36df404ea6c7448571bbdc9b9bfa508a
Guerreiro015 <antoniolucasluana@gmail.com>
2024-04-21 19:44:54 -0300

    atualkizar

---
+++ Carrinho de Compra/js/nav.js
@@ -0,0 +1,14 @@
+const closeIcon = document.querySelector(".close-icon");
+const openIcon = document.querySelector(".open-icon");
+const body = document.querySelector("body");
+const headerMobile = document.querySelector(".header-mobile");
+
+closeIcon.addEventListener("click", () => {
+    headerMobile.style.display = "none";
+    body.style.overflow = "scroll";
+});
+
+openIcon.addEventListener("click", () => {
+    headerMobile.style.display = "flex";
+    body.style.overflow = "hidden";
+});
