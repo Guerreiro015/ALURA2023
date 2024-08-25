4ee5c24ec872234da73bb6197d4c8f4838de2afc
Guerreiro015 <antoniolucasluana@gmail.com>
2024-08-25 19:37:30 -0300

    aualizar2508

--- SITE DE BUSCA/teste.js
+++ SITE DE BUSCA/teste.js
@@ -1,11 +1,44 @@
-fetch('banco.json')
-            .then(res => res.json())
-            .then((dados) => {
-                for (key in dados){
-                    console.log(key)
-                }
-            })
 
 
-            const chaves = Object.keys(meuObjeto);
-console.log(chaves); // ["nome", "idade", "cidade"]
+lista = [0]
+x=2
+y=5
+lista.push(x)
+lista.push(y)
+
+console.log(lista)
+console.log(lista[1])
+
+
+{/* <div class="totaisGerentes">
+            <div>
+                <fieldset>
+                    <legend>Totais Gerencia</legend>
+                    <h2 class="gerente">WILSON</h2>
+                    <h3 class="wilson_HE"></h3>
+                    <h3 class="wilson_Inter"></h3>
+                    <h3 class="wilson_Dsf"></h3>
+                </fieldset>
+            </div>
+            <div>
+                <fieldset>
+                    <legend>Totais Gerencia</legend>
+                    <h2 class="gerente">JOÃO SALES</h2>
+                    <h3 class="joao_HE"></h3>
+                    <h3 class="joao_Inter"></h3>
+                    <h3 class="joao_Dsf"></h3>
+                </fieldset>
+            </div>
+            <div>
+                <fieldset>
+                    <legend>Totais Gerencia</legend>
+                    <h2 class="gerente">GUSTAVO</h2>
+                    <h3 class="gustavo_HE"></h3>
+                    <h3 class="gustavo_Inter"></h3>
+                    <h3 class="gustavo_Dsf"></h3>
+                </fieldset>
+            </div>
+        </div>
+ */}
+
+
